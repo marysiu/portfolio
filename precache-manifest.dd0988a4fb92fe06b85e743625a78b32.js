0284f8e4b97b74b03e00a92c2686397475d42ae0
marysiu <blackenoughbro@gmail.com>
2022-02-22 19:34:35 +0800

    Updates

---
+++ precache-manifest.dd0988a4fb92fe06b85e743625a78b32.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8699d45da6993dadc2c9986321a6ef11",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a5391214d93b4dbeb0f4",
+    "url": "/portfolio/static/css/2.9c389199.chunk.css"
+  },
+  {
+    "revision": "c93f557a1f84df8721e6",
+    "url": "/portfolio/static/css/main.b4c4e056.chunk.css"
+  },
+  {
+    "revision": "a5391214d93b4dbeb0f4",
+    "url": "/portfolio/static/js/2.e8169e86.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/portfolio/static/js/2.e8169e86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93f557a1f84df8721e6",
+    "url": "/portfolio/static/js/main.a294e605.chunk.js"
+  },
+  {
+    "revision": "2fc01b149b6bfae91140",
+    "url": "/portfolio/static/js/runtime-main.2c9076bf.js"
+  },
+  {
+    "revision": "34899d8f79ab0ffd56a1466173c68c8b",
+    "url": "/portfolio/static/media/SIUHoiWa_CV.34899d8f.pdf"
+  },
+  {
+    "revision": "7d37cfd1a7a506105359b81f93468007",
+    "url": "/portfolio/static/media/UST.7d37cfd1.png"
+  },
+  {
+    "revision": "0d872a1e92176c051ebe86d2be24d1c1",
+    "url": "/portfolio/static/media/engineer.0d872a1e.svg"
+  },
+  {
+    "revision": "e2e1f6b8a3f831a9218066697ece65a1",
+    "url": "/portfolio/static/media/handyRehabIcon.e2e1f6b8.svg"
+  },
+  {
+    "revision": "8683213c4635801c5dd1d87135037a19",
+    "url": "/portfolio/static/media/matlab.8683213c.svg"
+  },
+  {
+    "revision": "25758dc07fe89064d321fa9e80b52139",
+    "url": "/portfolio/static/media/meiyumeLipstick.25758dc0.svg"
+  },
+  {
+    "revision": "c89529b02051461f4ea04658e77b8a24",
+    "url": "/portfolio/static/media/orii.c89529b0.png"
+  },
+  {
+    "revision": "926763ec4203310c411cbc70daafd356",
+    "url": "/portfolio/static/media/school.926763ec.svg"
+  },
+  {
+    "revision": "05021a606cd53ea3fa2b0a4331ac05af",
+    "url": "/portfolio/static/media/sportopia.05021a60.png"
+  },
+  {
+    "revision": "192b4e63abb716991d7db58ba509b2b8",
+    "url": "/portfolio/static/media/sportopiaIcon.192b4e63.svg"
+  }
+]);
